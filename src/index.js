c5a213766e1695e198a56d444e710f5596735ff5
Nando <ernandofebrian6@gmail.com>
2025-02-14 11:02:54 +0700

    Done making weather info content and background for the website.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 import "./styles.css";
 
+function storeWeatherInfo(array) {
+    const condition = document.querySelector(".conditions");
+    const location = document.querySelector(".location");
+    const temp = document.querySelector(".temprature");
+    const feels = document.querySelector(".feels");
+    const wind = document.querySelector(".wind");
+    const humidity = document.querySelector(".humidity");
+
+    condition.textContent = array[0];
+    location.textContent = array[1];
+    temp.textContent = ((array[2] - 32) / (9 / 5)).toFixed(1) + '°C';
+    feels.textContent = 'Feels Like:\t ' + ((array[3] - 32) / (9 / 5)).toFixed(1) + '°C';
+    wind.textContent = 'Wind: ' + array[4] + 'KM/H';
+    humidity.textContent = 'Humidity: ' + array[5] + '%';
+}
+
+
 async function getWeatherInfo(location) {
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=F2A2CFWBQHU6JJRNQ4N6SZU83`, {mode: 'cors'});
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=F2A2CFWBQHU6JJRNQ4N6SZU83`, { mode: 'cors' });
         const weatherData = await response.json();
         console.log(weatherData);
-        console.log(weatherData.address);
-        console.log(weatherData.currentConditions.temp);
-        console.log(weatherData.currentConditions.conditions);
-        console.log(weatherData.currentConditions.windspeed);
-        console.log(weatherData.currentConditions.humidity);
-        console.log(weatherData.currentConditions.feelslike);
         return weatherData
     } catch {
         console.log('error');
     }
 }
+async function makeScreenInfo(location) {
+    const data = [];
 
-getWeatherInfo("toronto");
+    try {
+        const currentData = await getWeatherInfo(location);
+        data.push(currentData.currentConditions.conditions);
+        data.push(currentData.resolvedAddress);
+        data.push(currentData.currentConditions.temp);
+        data.push(currentData.currentConditions.feelslike);
+        data.push(currentData.currentConditions.windspeed);
+        data.push(currentData.currentConditions.humidity);
+        storeWeatherInfo(data);
+    } catch {
+        console.log('error');
+    }
+}
 
-// async function makeScreenInfo(location) {
-//     try {
-//         const currentData = await getWeatherInfo(location);
-
-//     } catch {
-4//         console.log('error');
-//     }
-    
-// }
+makeScreenInfo("Jakarta");
